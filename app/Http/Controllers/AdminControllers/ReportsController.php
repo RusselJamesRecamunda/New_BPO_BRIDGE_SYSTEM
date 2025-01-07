0f298c6057e5dfc591f3d854ba009eac4ace8e0c
Russel Recamunda <130855654+RusselJamesRecamunda@users.noreply.github.com>
2025-01-07 13:24:34 +0800

    feat: enhance reports functionality to display new hires and document submissions

--- app/Http/Controllers/AdminControllers/ReportsController.php
+++ app/Http/Controllers/AdminControllers/ReportsController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers\AdminControllers;
 use App\Http\Controllers\Controller;
+use App\Models\Employees;
+use App\Models\InterviewResults;
+use Illuminate\Support\Facades\DB;
 
 use Illuminate\Http\Request;
 
@@ -11,10 +14,40 @@ class ReportsController extends Controller
      * Display a listing of the resource.
      */
     public function index()
-    {
-        //
-        return view('admin.reports');
-    }
+{
+    // Retrieve all employees with their related assets
+    $newHire = Employees::join('employee_assets', 'employees.emp_id', '=', 'employee_assets.emp_id')
+        ->select(
+            'employees.first_name',
+            'employees.last_name',
+            'employee_assets.emp_email',
+            'employee_assets.official_emp_id',
+            'employee_assets.project_department',
+            'employee_assets.dept_manager',
+            'employee_assets.hire_date',
+            'employee_assets.work_status'
+        )
+        ->get();
+    
+    // Retrieve documents and interview results based on result_id
+    $newDocument = InterviewResults::join('document_submissions', 'interview_results.result_id', '=', 'document_submissions.result_id')
+        ->select(
+            'interview_results.candidate_name',
+            'interview_results.applied_job',
+            DB::raw('`document_submissions`.`2x2_pic` as `2x2_pic`'),
+            DB::raw('`document_submissions`.`birth_certificate` as `birth_certificate`'),
+            DB::raw('`document_submissions`.`tin_number` as `tin_number`'),
+            DB::raw('`document_submissions`.`philhealth_id` as `philhealth_id`'),
+            DB::raw('`document_submissions`.`pagibig_membership_id` as `pagibig_membership_id`'),
+            DB::raw('`document_submissions`.`sss` as `sss`'),
+            DB::raw('`document_submissions`.`bir_form` as `bir_form`'),
+            DB::raw('`document_submissions`.`health_cert` as `health_cert`')
+        )
+        ->get();
+    
+    return view('admin.reports', compact('newHire', 'newDocument'));
+}
+
 
     /**
      * Show the form for creating a new resource.
--- resources/views/admin/reports.blade.php
+++ resources/views/admin/reports.blade.php
@@ -45,7 +45,6 @@
                             <th>Employee Name</th>
                             <th>Employee ID</th>
                             <th>Email Address</th>
-                            <th>Job Title</th>
                             <th>Type of Work</th>
                             <th>Company Department</th>
                             <th>Department Manager</th>
@@ -53,16 +52,17 @@
                         </tr>
                     </thead>
                     <tbody>
+                        @foreach($newHire as $hire)
                         <tr>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
+                            <td>{{$hire->first_name}} {{$hire->last_name}}</td>
+                            <td>{{$hire->official_emp_id}}</td>
+                            <td>{{$hire->emp_email}}</td>
+                            <td>{{$hire->work_status}}</td>
+                            <td>{{$hire->project_department}}</td>
+                            <td>{{$hire->dept_manager}}</td>
+                            <td>{{$hire->hire_date}}</td>
                         </tr>
+                        @endforeach
                     </tbody>
                 </table>
             </div>
@@ -88,22 +88,113 @@
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
+                        @foreach($newDocument as $document)
+                            <tr>
+                                <td>{{ $document->candidate_name }}</td>
+                                <td>{{ $document->applied_job }}</td>
+
+                                <!-- 2x2 Photo -->
+                                <td>
+                                    @if ($document['2x2_pic'])
+                                        <a href="{{ asset('storage/' . str_replace('public/', '', $document['2x2_pic'])) }}" target="_blank" download>
+                                            <i class="fa-solid fa-file-arrow-down text-danger fs-3"></i>
+                                            <p class="text-dark fw-light mt-2" style="font-size: 10px;">Download 2x2 Photo</p>
+                                        </a>
+                                    @else
+                                        <span>No 2x2 Photo</span>
+                                    @endif
+                                </td>
+
+                                <!-- Birth Certificate -->
+                                <td>
+                                    @if ($document['birth_certificate'])
+                                        <a href="{{ asset('storage/' . str_replace('public/', '', $document['birth_certificate'])) }}" target="_blank" download>
+                                            <i class="fa-solid fa-file-arrow-down text-danger fs-3"></i>
+                                            <p class="text-dark fw-light mt-2" style="font-size: 10px;">Download Birth Certificate</p>
+                                        </a>
+                                    @else
+                                        <span>No Birth Certificate</span>
+                                    @endif
+                                </td>
+
+                                <!-- Tin Number -->
+                                <td>
+                                    @if ($document['tin_number'])
+                                        <a href="{{ asset('storage/' . str_replace('public/', '', $document['tin_number'])) }}" target="_blank" download>
+                                            <i class="fa-solid fa-file-arrow-down text-danger fs-3"></i>
+                                            <p class="text-dark fw-light mt-2" style="font-size: 10px;">Download Tin Number</p>
+                                        </a>
+                                    @else
+                                        <span>No Tin Number</span>
+                                    @endif
+                                </td>
+
+                                <!-- PhilHealth -->
+                                <td>
+                                    @if ($document['philhealth_id'])
+                                        <a href="{{ asset('storage/' . str_replace('public/', '', $document['philhealth_id'])) }}" target="_blank" download>
+                                            <i class="fa-solid fa-file-arrow-down text-danger fs-3"></i>
+                                            <p class="text-dark fw-light mt-2" style="font-size: 10px;">Download PhilHealth</p>
+                                        </a>
+                                    @else
+                                        <span>No PhilHealth</span>
+                                    @endif
+                                </td>
+
+                                <!-- Pag-Ibig -->
+                                <td>
+                                    @if ($document['pagibig_membership_id'])
+                                        <a href="{{ asset('storage/' . str_replace('public/', '', $document['pagibig_membership_id'])) }}" target="_blank" download>
+                                            <i class="fa-solid fa-file-arrow-down text-danger fs-3"></i>
+                                            <p class="text-dark fw-light mt-2" style="font-size: 10px;">Download Pag-Ibig</p>
+                                        </a>
+                                    @else
+                                        <span>No Pag-Ibig</span>
+                                    @endif
+                                </td>
+
+                                <!-- SSS -->
+                                <td>
+                                    @if ($document['sss'])
+                                        <a href="{{ asset('storage/' . str_replace('public/', '', $document['sss'])) }}" target="_blank" download>
+                                            <i class="fa-solid fa-file-arrow-down text-danger fs-3"></i>
+                                            <p class="text-dark fw-light mt-2" style="font-size: 10px;">Download SSS</p>
+                                        </a>
+                                    @else
+                                        <span>No SSS</span>
+                                    @endif
+                                </td>
+
+                                <!-- BIR Form -->
+                                <td>
+                                    @if ($document['bir_form'])
+                                        <a href="{{ asset('storage/' . str_replace('public/', '', $document['bir_form'])) }}" target="_blank" download>
+                                            <i class="fa-solid fa-file-arrow-down text-danger fs-3"></i>
+                                            <p class="text-dark fw-light mt-2" style="font-size: 10px;">Download BIR Form</p>
+                                        </a>
+                                    @else
+                                        <span>No BIR Form</span>
+                                    @endif
+                                </td>
+
+                                <!-- Health Certificate -->
+                                <td>
+                                    @if ($document['health_cert'])
+                                        <a href="{{ asset('storage/' . str_replace('public/', '', $document['health_cert'])) }}" target="_blank" download>
+                                            <i class="fa-solid fa-file-arrow-down text-danger fs-3"></i>
+                                            <p class="text-dark fw-light mt-2" style="font-size: 10px;">Download Health Certificate</p>
+                                        </a>
+                                    @else
+                                        <span>No Health Certificate</span>
+                                    @endif
+                                </td>
+                            </tr>
+                        @endforeach
                     </tbody>
                 </table>
             </div>
         </div>
+
     </div>
 
 @endsection
