93675c8ea4279b77256265b8a479d31f3927dc95
Russel Recamunda <130855654+RusselJamesRecamunda@users.noreply.github.com>
2025-01-08 21:02:47 +0800

    feat: add RoleSeeder to DatabaseSeeder for inserting admin and applicant roles

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,5 +24,8 @@ class DatabaseSeeder extends Seeder
 
         // Call the CategorySeeder to insert job categories
         $this->call(CategorySeeder::class);
+
+        // Call the RoleSeeder to insert admin and applicant roles
+        $this->call(RoleSeeder::class);
     }
 }
